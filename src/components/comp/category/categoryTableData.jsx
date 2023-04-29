196c4e3bdebc34a337d08e413df7768d68b123e5
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-04-29 18:43:40 +0500

    Category Table

--- src/components/comp/category/categoryTableData.jsx
+++ src/components/comp/category/categoryTableData.jsx
@@ -2,6 +2,7 @@
 import { ManOutlined } from "@ant-design/icons";
 import { BiEdit } from "react-icons/bi";
 import { MdDelete } from "react-icons/md";
+import {CiBeerMugFull, CiBurger, CiLaptop} from "react-icons/ci";
 
 
 export const Headings=["ID","Name","Slug", "Icon", "Group", "Action"]; 
@@ -16,7 +17,140 @@ export const catTableData=[
             <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
             
         ],
-      }
+      },
+      {
+        id:"2",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"3",
+        Name:"Burger",
+        Slug:"Food",
+        Icon: <CiBurger/>,
+        Group: "Food",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"4",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"5",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"6",
+        Name:"Beer",
+        Slug:"Drinks",
+        Icon: <CiBeerMugFull/>,
+        Group: "Beverage",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"7",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"8",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"9",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"10",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"11",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"12",
+        Name:"Pants",
+        Slug:"Pants",
+        Icon: <ManOutlined/>,
+        Group: "Clothes",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+      {
+        id:"13",
+        Name:"Laptops",
+        Slug:"Electronics",
+        Icon: <CiLaptop/>,
+        Group: "Mobiles and Laptops",
+        Action:[
+            <MdDelete size={25} color="red"/> ,<BiEdit size={25} />
+            
+        ],
+      },
+
     ];
 
   
