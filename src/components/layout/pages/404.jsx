bc3ce122e422479ba126c6a8f9f18ab7762641fc
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-05-16 15:56:17 +0500

    PARENT CATEGORY REMOVED

--- src/components/layout/pages/404.jsx
+++ src/components/layout/pages/404.jsx
@@ -1,16 +1,19 @@
-import React from 'react'
+import React from "react";
 const NotFound = () => {
   return (
     <>
       <div className="fixed inset-0 flex items-center justify-center">
-    <div className="flex flex-col items-center">
-      <span className="mt-4 text-black text-lg font-semibold">404 Error</span>
-      <span className="mt-4 text-gray-500 text-lg font-semibold">
-        Its Look like the  Page Doesn't Exist</span>
-    </div>
-  </div>
-  </>
-  )
-}
+        <div className="flex flex-col items-center">
+          <span className="mt-4 text-[#248F59] text-lg font-serif font-semibold">
+            404 Error
+          </span>
+          <span className="mt-4 text-gray-500 font-sans text-lg font-semibold">
+            Its Look like the Page Doesn't Exist
+          </span>
+        </div>
+      </div>
+    </>
+  );
+};
 
-export default NotFound
+export default NotFound;
