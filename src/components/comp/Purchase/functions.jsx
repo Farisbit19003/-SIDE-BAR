b56f4a18ab968e4adbac3a498f92465deddd76c2
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-05-27 23:28:35 +0500

    INVOICE N PURCHASE

--- src/components/comp/Purchase/functions.jsx
+++ src/components/comp/Purchase/functions.jsx
@@ -3,3 +3,8 @@ export const CreateOrder = async (values) => {
   const { data } = await axios.post("/order/create-purchase", {values});
   return data;
 };
+
+export const DeleteProduct = async (values) => {
+  const { data } = await axios.post(`/product/delete/${values?.slug}`,{values});
+  return data;
+};
--- src/components/comp/orders/order-detail.jsx
+++ src/components/comp/orders/order-detail.jsx
@@ -1,4 +1,4 @@
-import React, { useState,useEffect} from "react";
+import React, { useState, useEffect } from "react";
 import OrderViewHeader from "./orderdetail/order-view-header";
 import ShopLayout from "../../layout/Shop";
 import DetailPack from "./orderdetail/detailpack";
@@ -6,22 +6,23 @@ import { OrderDetailTable } from "./orderdetail/orderDetailTable";
 import { useSelector } from "react-redux";
 import { AiOutlineLoading3Quarters } from "react-icons/ai";
 import { Link, useNavigate, useParams } from "react-router-dom";
-const handleDelete=()=>{
+const handleDelete = () => {};
+const OrderDetail = () => {
+  const { allOrders, loggedIn } = useSelector((state) => ({ ...state }));
+  const [singleOrder, setSingleOrder] = useState({});
+  const params = useParams();
+  const navigate = useNavigate();
+  let GrandTotal =
+    singleOrder?.orderType == "Sales"
+      ? singleOrder?.Products?.reduce((acc, p) => {
+          return acc + p?.Product.salePrice * p.order_quantity;
+        }, 0)
+      : singleOrder?.Products?.reduce((acc, p) => {
+          return acc + p?.Product.purchasePrice * p.order_quantity;
+        }, 0);
 
-}
-const OrderDetail=()=>{
-  const {allOrders,loggedIn}=useSelector((state)=>({...state}));
-  const [singleOrder,setSingleOrder]=useState({});
-  const params=useParams();
-  const navigate=useNavigate();
-  let GrandTotal =singleOrder?.orderType=="Sales"?singleOrder?.Products?.reduce((acc, p) => {
-    return acc + (p?.Product.salePrice * p.order_quantity);
-  }, 0):singleOrder?.Products?.reduce((acc, p) => {
-    return acc + (p?.Product.purchasePrice * p.order_quantity);
-  }, 0);
- 
   const LoadOrder = () => {
-    const updated =allOrders?.filter((o) => {
+    const updated = allOrders?.filter((o) => {
       return params._id === o._id;
     });
     setSingleOrder(updated[0]);
@@ -30,35 +31,35 @@ const OrderDetail=()=>{
     if (allOrders && allOrders.length) {
       LoadOrder();
     }
-  }, [params,allOrders]);
+  }, [params, allOrders]);
   useEffect(() => {
-    if (!singleOrder|| !loggedIn?.token) {
+    if (!singleOrder || !loggedIn?.token) {
       const timeoutId = setTimeout(() => {
         navigate("/");
       }, 3000);
       return () => clearTimeout(timeoutId);
     }
-  }, [singleOrder,loggedIn]);
-    return !singleOrder || allOrders===null || !loggedIn?.token ? (
-      <div className="h-screen flex items-center justify-center">
-        <div className="flex flex-col items-center">
-          <AiOutlineLoading3Quarters className="text-6xl w-16 h-16 text-[#248F59] animate-spin" />
-          <span className="mt-4 text-gray-500 text-lg font-semibold">
-            Loading...
-          </span>
-        </div>
+  }, [singleOrder, loggedIn]);
+  return !singleOrder || allOrders === null || !loggedIn?.token ? (
+    <div className="h-screen flex items-center justify-center">
+      <div className="flex flex-col items-center">
+        <AiOutlineLoading3Quarters className="text-6xl w-16 h-16 text-[#248F59] animate-spin" />
+        <span className="mt-4 text-gray-500 text-lg font-semibold">
+          Loading...
+        </span>
       </div>
-    ): (
-      <>
-        <ShopLayout>
-          <OrderViewHeader singleOrder={singleOrder} />
-          <OrderDetailTable
-            handleDelete={handleDelete}
-           singleOrder={singleOrder}
-          />
-          <DetailPack singleOrder={singleOrder} GrandTotal={GrandTotal} />
-        </ShopLayout>
-      </>
-    );
-}
+    </div>
+  ) : (
+    <>
+      <ShopLayout>
+        <OrderViewHeader singleOrder={singleOrder} GrandTotal={GrandTotal} />
+        <OrderDetailTable
+          handleDelete={handleDelete}
+          singleOrder={singleOrder}
+        />
+        <DetailPack singleOrder={singleOrder} GrandTotal={GrandTotal} />
+      </ShopLayout>
+    </>
+  );
+};
 export default OrderDetail;
