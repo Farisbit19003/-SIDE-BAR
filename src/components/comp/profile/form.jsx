2ab2a551a4fda454ae450e6f16bfa69757ff5e38
Kamran Ali <Kamran Ali@KAMRAN-ALI>
2023-06-01 01:26:33 +0500

    reports

--- src/components/comp/profile/form.jsx
+++ src/components/comp/profile/form.jsx
@@ -6,7 +6,7 @@ import SaveButton from "../../comp/common/save";
 import { toast } from "react-toastify";
 import { BecomeSeller, UpdateProfile } from "../../auth/auth";
 import { LoadingOutlined } from "@ant-design/icons";
-const Form = ({dispatch,loggedIn,avatarInfo }) => {
+const Form = ({ dispatch, loggedIn, avatarInfo }) => {
   const [email, setEmail] = useState("");
   const [whatsapp, setWhatsapp] = useState("");
   const [name, setName] = useState("");
@@ -14,7 +14,8 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
   const [Conpassword, setConPassword] = useState("");
   const [stripe_account_id, setStripe_account_id] = useState("");
   const [image, setImage] = useState([]);
-
+  const [passwordError, setPasswordError] = useState("");
+  const [whatsappError, setWhatsappError] = useState("");
   const [loading, setloading] = useState(false);
 
   useEffect(() => {
@@ -23,7 +24,7 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
       setName(loggedIn.user.name);
       setWhatsapp(loggedIn.user.whatsapp);
       setStripe_account_id(loggedIn.user?.stripe_account_id);
-      loggedIn.user.image&&setImage([loggedIn.user.image]);
+      loggedIn.user.image && setImage([loggedIn.user.image]);
     }
   }, [loggedIn && loggedIn.token]);
   const handleSubmit = (e) => {
@@ -32,8 +33,11 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
       if (!name) {
         return toast.error("Please Enter Your Name");
       }
-      if (password&&password.length < 6) {
-        return toast.error("Please Enter Password atleast 6 characters");
+      if (passwordError) {
+        return toast.error("Please Enter valid Password");
+      }
+      if (whatsappError) {
+        return toast.error("Please Enter valid Contact Number");
       }
       if (password !== Conpassword) {
         return toast.error("Password Does't match");
@@ -50,12 +54,12 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
           window.localStorage.setItem("auth", JSON.stringify(auth));
           //update in state
           dispatch({
-            type:"LOGGED_IN_USER",
-            payload:{
+            type: "LOGGED_IN_USER",
+            payload: {
               ...loggedIn,
-              user:auth.user,
-            }
-          })
+              user: auth.user,
+            },
+          });
           setloading(false);
           toast.success("User Updated");
         }
@@ -66,7 +70,7 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
     }
   };
 
-  const handleStripe=(e)=>{
+  const handleStripe = (e) => {
     e.preventDefault();
     try {
       setloading(true);
@@ -75,15 +79,49 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
           setloading(false);
           toast.error(res.error);
         } else {
-          window.location.href=res.url;
+          window.location.href = res.url;
           setloading(false);
         }
       });
     } catch (error) {
       toast.error(error);
       setloading(false);
-    } 
-  }
+    }
+  };
+  const handlePasswordChange = (e) => {
+    const input = e.target.value;
+    setPassword(input);
+
+    // Validate password
+    const regex =
+      /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/;
+    if (!regex.test(input)) {
+      setPasswordError(
+        "Password must be at least 8 characters long and contain at least one special character and number."
+      );
+    } else {
+      setPasswordError("");
+    }
+  };
+  const handleWhatsappChange = (e) => {
+    const input = e.target.value;
+
+    // Remove any non-digit characters
+    const digitsOnly = input.replace(/\D/g, "");
+
+    // Limit the input to a maximum of 11 digits
+    const limitedInput = digitsOnly.slice(0, 11);
+
+    setWhatsapp(limitedInput);
+
+    // Validate Pakistan phone number
+    const regex = /^(\+92|0)?[0-9]{10}$/;
+    if (!regex.test(limitedInput)) {
+      setWhatsappError("Please enter a valid Pakistan phone number.");
+    } else {
+      setWhatsappError("");
+    }
+  };
   return (
     <>
       <form>
@@ -118,8 +156,13 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
           </div>
 
           <Card>
-            <FileInput loading={loading}
-             setImage={setImage} setloading={setloading} image={image} keyPrefix={"sixth"} />
+            <FileInput
+              loading={loading}
+              setImage={setImage}
+              setloading={setloading}
+              image={image}
+              keyPrefix={"sixth"}
+            />
           </Card>
         </div>
         {/* INFORMATION */}
@@ -136,40 +179,50 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
               <label className="font-semibold ">Name</label>
               <input
                 value={name}
-                onChange={(e) => setName(e.target.value)}
+                onChange={(e) => {
+                  const input = e.target.value;
+                  const regex = /^[a-zA-Z\s]*$/; // Regex to match alphabetic characters and spaces
+                  if (regex.test(input)) {
+                    setName(input);
+                  }
+                }}
                 type="text"
                 className="h-10 my-2  bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-green-600"
               />
               <label className="font-semibold ">Contact</label>
               <input
                 value={whatsapp}
-                onChange={(e) => setWhatsapp(e.target.value)}
+                onChange={handleWhatsappChange}
                 type="number"
                 className="h-10 my-2  bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-green-600"
               />
-               {loggedIn?.user?.stripeSeller?(<> 
-              <label className="font-semibold ">Stripe Payment ID</label>
-              <input
-               disabled
-                value={stripe_account_id}
-                className="h-10 my-2  bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-green-600"
-              />
-          
-            </>
-            ):
-            (<> 
-              <label className="font-semibold ">Stripe Account Setup</label>
-              <button
-              onClick={handleStripe}
-              className="bg-[#248F59] mt-2 text-white py-3 px-3 rounded">
-                 
-                 {loading?<LoadingOutlined/>: "  Complete Stripe OnBoarding"}
-                 
-                </button>
-            </>
-            )
-            }
-              </div>
+              {whatsappError && <p className="text-red-500">{whatsappError}</p>}
+
+              {loggedIn?.user?.stripeSeller ? (
+                <>
+                  <label className="font-semibold ">Stripe Payment ID</label>
+                  <input
+                    disabled
+                    value={stripe_account_id}
+                    className="h-10 my-2  bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-green-600"
+                  />
+                </>
+              ) : (
+                <>
+                  <label className="font-semibold ">Stripe Account Setup</label>
+                  <button
+                    onClick={handleStripe}
+                    className="bg-[#248F59] mt-2 text-white py-3 px-3 rounded"
+                  >
+                    {loading ? (
+                      <LoadingOutlined />
+                    ) : (
+                      "  Complete Stripe OnBoarding"
+                    )}
+                  </button>
+                </>
+              )}
+            </div>
           </Card>
         </div>
         {/* Password */}
@@ -185,10 +238,12 @@ const Form = ({dispatch,loggedIn,avatarInfo }) => {
               <label className="font-semibold ">New Password</label>
               <input
                 value={password}
-                onChange={(e) => setPassword(e.target.value)}
+                onChange={handlePasswordChange}
                 type="password"
                 className=" my-2 h-10 bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-green-600"
               />
+              {passwordError && <p className="text-red-500 text-xs">{passwordError}</p>}
+
               <label className="font-semibold ">Confirm Password</label>
               <input
                 value={Conpassword}
