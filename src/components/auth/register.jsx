57c65a65aca933431e61c65041a98e33ba8837a3
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-09-25 16:07:28 +0500

    PLEASE Chl jana

--- src/components/auth/register.jsx
+++ src/components/auth/register.jsx
@@ -1,6 +1,5 @@
 import { LoadingOutlined } from "@ant-design/icons";
-import { useContext, useEffect, useState } from "react";
-import { FaSpinner } from "react-icons/fa";
+import { useEffect, useState } from "react";
 import { useSelector } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
@@ -14,76 +13,51 @@ const Register = () => {
   const [password, setPassword] = useState("");
   const [loading, setloading] = useState(false);
   const { loggedIn } = useSelector((state) => ({ ...state }));
-  const [emailError, setEmailError] = useState("");
-  const [passwordError, setPasswordError] = useState("");
   const navigate = useNavigate();
+
   const handleSubmit = async (e) => {
     e.preventDefault();
     try {
-      if (!name || !email || !password) {
-        return toast.error("Please Fill al Field");
-      }
-      if (emailError || passwordError) {
-        return toast.error("Please Add Valid Email and Password");
-      }
       setloading(true);
-      createSeller(name, email, password).then((res) => {
-        if (res.error) {
-          setloading(false);
-          toast.error(res.error);
-        } else {
-          setloading(false);
-          setEmail("");
-          setName("");
-          setPassword("");
-          window.localStorage.setItem("Email", email);
-          swal("Please Check your Email and Complete Resgistration");
+      const res = await createSeller(name, email, password);
+      if (res.error) {
+        setloading(false);
+        // Display error messages in toast notifications
+        if (res.error.name) {
+          toast.error(res.error.name);
         }
-      });
+        if (res.error.email) {
+          toast.error(res.error.email);
+        }
+        if (res.error.password) {
+          toast.error(res.error.password);
+        }
+      } else {
+        setloading(false);
+        setEmail("");
+        setName("");
+        setPassword("");
+        window.localStorage.setItem("Email", email);
+        swal("Please Check your Email and Complete Registration");
+      }
     } catch (err) {
-      toast.error(err.response.data);
+      console.error("Error:", err);
+      toast.error("An error occurred while processing your request.");
       setloading(false);
     }
   };
+  
+  
+  
+  
   useEffect(() => {
     if (loggedIn && loggedIn.token) {
       setTimeout(() => {
         navigate("/");
-      }, 3000); // 5 seconds
+      }, 3000); // 3 seconds
     }
   }, [loggedIn && loggedIn.token]);
-  const validateEmail = (email) => {
-    // Regular expression pattern for email validation
-    const emailPattern = /^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i;
 
-    return emailPattern.test(email);
-  };
-  const handleEmailChange = (e) => {
-    const enteredEmail = e.target.value;
-    setEmail(enteredEmail);
-
-    if (enteredEmail && !validateEmail(enteredEmail)) {
-      setEmailError("Invalid email");
-    } else {
-      setEmailError("");
-    }
-  };
-
-  const handlePasswordChange = (e) => {
-    const input = e.target.value;
-    setPassword(input);
-
-    // Validate password
-    const regex =
-      /^(?=.*?[A-Za-z])(?=.*?[0-9])(?=.*?[!@#$%^&*])[A-Za-z0-9!@#$%^&*]{8,}$/;
-    if (!regex.test(input)) {
-      setPasswordError(
-        "Password must be at least 8 characters long and contain at least one special character and number."
-      );
-    } else {
-      setPasswordError("");
-    }
-  };
   return loggedIn && loggedIn.token ? (
     <>
       <div className="fixed inset-0 flex items-center justify-center">
@@ -97,53 +71,43 @@ const Register = () => {
     </>
   ) : (
     <>
-      <div className="bg-gray-200 flex flex-wrap h-screen lg:p-4  mx-auto justify-center">
+      <div className="bg-gray-200 flex flex-wrap h-screen lg:p-4 mx-auto justify-center">
         <div className="bg-white flex flex-col p-8 md:w-4/12 w-full mx-auto border-2 justify-center shadow">
           <Logo />
           <h1 className="text-gray-400 font-thin flex justify-center items-center italic mb-6 font-sans">
             Register New Account
           </h1>
           {/* NAME */}
-          <label className="mb-3 block font-sans text-sm font-semibold leading-none ">
+          <label className="mb-3 block font-sans text-sm font-semibold leading-none">
             Name
           </label>
           <input
             type="text"
             value={name}
-            onChange={(e) => {
-              const input = e.target.value;
-              const regex = /^[a-zA-Z\s]*$/; // Regex to match alphabetic characters and spaces
-              if (regex.test(input)) {
-                setName(input);
-              }
-            }}
+            onChange={(e) => setName(e.target.value)}
             className="h-12 mb-4 flex flex-wrap bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-[#248f59]"
           />
           {/* EMAIL */}
-          <label className="mb-3 block text-sm font-sans font-semibold leading-none ">
+          <label className="mb-3 block text-sm font-sans font-semibold leading-none">
             Email
           </label>
           <input
             type="email"
             value={email}
-            onChange={handleEmailChange}
+            onChange={(e) => setEmail(e.target.value)}
             className="h-12 mb-4 flex flex-wrap bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-[#248f59]"
           />
-          {emailError && <p className="text-red-500 font-sans">{emailError}</p>}
 
           {/* PASSWORD */}
-          <label className="font-semibold font-sans flex flex-wrap mb-3 text-sm leading-none ">
+          <label className="font-semibold font-sans flex flex-wrap mb-3 text-sm leading-none">
             Password
           </label>
           <input
             type="password"
             value={password}
-            onChange={handlePasswordChange}
+            onChange={(e) => setPassword(e.target.value)}
             className="h-12 mb-4 flex flex-wrap bg-white border border-gray-400 rounded-lg px-3 py-2 text-lg font-sans font-normal tracking-normal text-left focus:outline-none focus:ring-2 focus:ring-[#248f59]"
           />
-          {passwordError && (
-            <p className="text-red-500 font-sans">{passwordError}</p>
-          )}
 
           {/* BUTTON */}
           <button
@@ -153,7 +117,7 @@ const Register = () => {
             {loading ? <LoadingOutlined /> : " Register"}
           </button>
           {/* OR */}
-          <div className="relative flex flex-col mt-2 items-center justify-center text-sm ">
+          <div className="relative flex flex-col mt-2 items-center justify-center text-sm">
             <hr className="w-full" />
             <span className="start-2/4 -ms-4 absolute font-sans -top-2.5 bg-white px-2">
               OR
