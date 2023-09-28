d57cba95403b6658899b27bd762a8376761c694d
Farisbit19003 <farisirfanbit19.03@gmail.com>
2023-09-28 11:18:34 +0500

    NPM I KERNA

--- src/components/Products/addProduct.jsx
+++ src/components/Products/addProduct.jsx
@@ -2,11 +2,11 @@ import React, { useEffect, useState } from "react";
 import { useDispatch, useSelector } from "react-redux";
 import { useNavigate } from "react-router-dom";
 import { toast } from "react-toastify";
+import { PostFunction } from "../../Helper/Service";
 import ShopLayout from "../../layout/Shop/index";
 import SaveButton from "../common/save";
 import ProductForm from "./ProductForm";
-import { CreateProduct, SellerProducts } from "./functions";
-
+import { SellerProducts } from "./functions";
 const AddProduct = () => {
   const [shops, setShops] = useState([]);
   const { sellerShops } = useSelector((state) => ({ ...state }));
@@ -23,64 +23,54 @@ const AddProduct = () => {
     feature_pic: {},
   });
   const [loading, setLoading] = useState(false);
-  
+
   const navigate = useNavigate();
   const dispatch = useDispatch();
-  
+
   useEffect(() => {
     if (sellerShops) {
       setShops(sellerShops);
     }
   }, [sellerShops]);
-  
+
   //handleSubmit
   const handleSubmit = (e) => {
     e.preventDefault();
-    if (
-      !values.name ||
-      !values.discription ||
-      !values.store ||
-      !values.category ||
-      !values.salePrice ||
-      !values.purchasePrice ||
-      !values.quantity ||
-      !values.feature_pic
-    ) {
-      toast.error("Please Fill all Fields");
-      setLoading(false);
-      return;
-    }
-    if (values.gallery_pics.length < 3) {
-      toast.error("Please add at least Three Gallery Images");
-      setLoading(false);
-      return;
-    }
-    if (Math.round( values.purchasePrice) >= Math.round (values.salePrice)) {
-      toast.error("Purchase Price must be smaller than Sale Price");
-      setLoading(false);
-      return;
-    }
-    if (values.purchasePrice <= 0) {
-      toast.error("Purchase Price must be greater than zero");
-      setLoading(false);
-      return;
-    }
-    if (values.quantity <= 0) {
-      toast.error("Please enter a value greater than zero for the quantity.");
-      setLoading(false);
-      return;
-    }
-    if (values.salePrice <= 0) {
-      toast.error("Sale Price must be greater than zero");
-      setLoading(false);
-      return;
-    }
     try {
       setLoading(true);
-      CreateProduct(values).then((res) => {
-        if (res.error) {
-          toast.error(res.error);
+      let obj = {
+       values
+      };
+      PostFunction("/product/create", obj).then((res) => {
+        if (res.hasError) {
           setLoading(false);
+          if (res.error.salePrice) {
+            toast.error.salePrice;
+          }
+          if (res.error.store) {
+            toast.error.store;
+          }
+          if (res.error.category) {
+            toast.error.category;
+          }
+          if (res.error.name) {
+            toast.error.name;
+          }
+          if (res.error.gallery_pics) {
+            toast.error.gallery_pics;
+          }
+          if (res.error.feature_pic) {
+            toast.error.feature_pic;
+          }
+          if (res.error.quantity) {
+            toast.error.quantity;
+          }
+          if (res.error.unit) {
+            toast.error.unit;
+          }
+          if (res.error.purchasePrice) {
+            toast.error.purchasePrice;
+          }
         } else {
           toast.success("Product Created");
           setValues({
